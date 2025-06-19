d718b50c376e1424c029dcf27e70b851dbdfa81b
David Udoh <udohdavid004@gmail.com>
2025-06-19 14:15:14 +0100

    test framermotion

--- app/(user)/components/BottomModal.tsx
+++ app/(user)/components/BottomModal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { motion, AnimatePresence } from "motion/react";
+import { motion, AnimatePresence } from "framer-motion";
 import { useRef } from "react";
 
 type Props = {
--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -4,14 +4,17 @@ import Header from "./components/home/Header";
 import Play from "./components/home/Play";
 import Wallet from "./components/home/Wallet";
 import MenuSlider from "./components/MenuSlider";
+import Page from "./components/MotionTest";
 
 export default function Home() {
-  return (
-    <MenuSlider>
-      <Header text="Wallet" />
-      <Wallet />
-      <Header text="Play" />
-      <Play />
-    </MenuSlider>
-  );
+  // return (
+  //   <MenuSlider>
+  //     <Header text="Wallet" />
+  //     <Wallet />
+  //     <Header text="Play" />
+  //     <Play />
+  //   </MenuSlider>
+  // );
+
+  return <Page></Page>;
 }
